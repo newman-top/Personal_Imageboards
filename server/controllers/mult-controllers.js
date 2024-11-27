b2bc388c5b4d8eb2e2428ad84416ac882700d8d5
newman-top <marcin.korbas.jav@gmail.com>
2024-11-27 15:13:33 -0600

    Moved callback func to mult-controllers.js

--- server/controllers/mult-controllers.js
+++ server/controllers/mult-controllers.js
@@ -1,7 +1,7 @@
 const multer  = require('multer');
 const { GridFsStorage } = require('multer-gridfs-storage');
 
-function upload_img(collection) {
+function upload(collection) {
     const storage = new GridFsStorage({
         url: process.env.CLUSTER,
         file: (req, file) => { // TODO - These args aren't being used ... do I need them here?
@@ -16,6 +16,16 @@ function upload_img(collection) {
     return multer({ storage });
 }
 
+const upload_banner = async (req, res) => {
+    console.log(req.file.id); // Looks like this works thank god ... I can reference the new file by mongoID
+    // It might just make sense here to create a new field in the user model which references the banner by ID
+        // Since that's all I need to retrieve it ... that should make the logic easy to manage in theory
+        // TODO - I will need to attach a reference to the user to the request before sending it
+            // Or else I will not be able to put the image ID into the corresponding user doc
+    res.status(201).json({message: "Image successfully uploaded"});
+}
+
 module.exports = {
-    upload_img
+    upload,
+    upload_banner
 }
--- server/routes/mult-routes.js
+++ server/routes/mult-routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    upload_img
+    upload,
+    upload_banner
 } = require("../controllers/mult-controllers");
 
 // TODO - Just making a note here cause I will need to handle this later ... MUST check that the file is an IMAGE before allowing any uploads
@@ -16,13 +17,6 @@ const {
         // And move the second callback to controllers
         // Since it is shaping up to handle the controller logic anyways
             // i.e. multer middleware uploads, then my controller handles the logic from there (with the uploaded file's ID now accessible from req)
-router.post("/upload/banner", upload_img("banners").single("file"), (req, res) => {
-    console.log(req.file.id); // Looks like this works thank god ... I can reference the new file by mongoID
-    // It might just make sense here to create a new field in the user model which references the banner by ID
-        // Since that's all I need to retrieve it ... that should make the logic easy to manage in theory
-        // TODO - I will need to attach a reference to the user to the request before sending it
-            // Or else I will not be able to put the image ID into the corresponding user doc
-    res.status(201).json({message: "Image successfully uploaded"});
-});
+router.post("/upload/banner", upload("banners").single("file"), upload_banner);
 
 module.exports = router;
