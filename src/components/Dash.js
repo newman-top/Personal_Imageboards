822f9773646a745ba7f9ca46a7fc087582d30b1c
newman-top <marcin.korbas.jav@gmail.com>
2024-12-05 21:29:20 -0600

    Connected banner upload modal to control panel

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -4,16 +4,13 @@ import { useState } from "react";
 import Banner from "./Banner";
 
 import NiceModal from '@ebay/nice-modal-react';
-// import TestModal from "./modals/BannerUpload"; // TEMP
 import ControlPanel from "./modals/ControlPanel";
 
 const Dash = () => {
 
     const { user } = useAuthContext();
-    // let [isOpen, setIsOpen] = useState(false);
 
     const on_click_banner_upload = (e) => {
-        // NiceModal.show(TestModal, {name: "Test"});
         NiceModal.show(ControlPanel);
     }
 
--- src/components/modals/ControlPanel.js
+++ src/components/modals/ControlPanel.js
@@ -1,7 +1,17 @@
 import NiceModal, { useModal } from '@ebay/nice-modal-react';
+import BannerUpload from "./BannerUpload";
 
 export default NiceModal.create((name) => {
     const modal = useModal();
+
+    const on_click_banner_upload = (e) => {
+        NiceModal.show(BannerUpload);
+    }
+
+    const on_click_post_image = (e) => {
+        // TODO
+    }
+
     return (
         <div className="control-panel">
             <div className="panel-list">
@@ -10,10 +20,10 @@ export default NiceModal.create((name) => {
                         filter
                     </span>
                 </div>
-                <div className="panel-entry">
+                <div className="panel-entry" onClick={on_click_banner_upload}>
                     <p>Upload banner image</p>
                 </div>
-                <div className="panel-entry">
+                <div className="panel-entry" onClick={on_click_post_image}>
                     <p>Post image to board</p>
                 </div>
                 <div className="panel-bookend"></div>
