a8c9ef757e8b3b88f1f07169c2dadc257794acc2
newman-top <marcin.korbas.jav@gmail.com>
2024-12-11 14:52:22 -0600

    Implemented hook for booru image uploads

--- src/components/modals/ImageUpload.js
+++ src/components/modals/ImageUpload.js
@@ -5,18 +5,25 @@ import { useImageUpload } from "../../hooks/useImageUpload";
 export default NiceModal.create((name) => {
     const modal = useModal();
 
+    const { image_upload, error, pending } = useImageUpload();
+
     const submit = async (e) => {
         e.preventDefault();
-        // TODO
+        const input = document.getElementById("image-file");
+        const tags = document.getElementById("image-tags");
+        await image_upload(input.files[0], tags);
+        window.location.reload();
     }
 
     return (
         <div className="image-modal">
             <h3>image-modal</h3>
             <form onSubmit={submit}>
-                <label>Select file:</label>
+                <label>Select file: </label>
                 <input type="file" className="image-file-select" id="image-file"/> <br />
-                <button className="image-modal-btn">Upload banner</button>
+                <label>Tags: </label>
+                <input type="text" id="image-tags" />
+                <button className="image-modal-btn">Post image</button>
             </form>
         </div>
     );
--- src/hooks/useImageUpload.js
+++ src/hooks/useImageUpload.js
@@ -1,4 +1,30 @@
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useState } from "react";
 
-export const useImageUpload = (file) => {}
+export const useImageUpload = (file, tags) => {
+    // Initializing states (err & pending)
+    const [error, setError] = useState(null);
+    const [pending, setPending] = useState(null);
+    // Pulling necessary refs from context provider
+    const { user } = useAuthContext();
+    // TODO - May tweak the below code in the future to handle uploads of multiple files at once
+    const image_upload = async (file) => {
+        setPending(true); // Set pending
+        setError(null); // Reset errors
+        const data = new FormData();
+        data.append('file', file);
+        data.append('tags', tags); // Tags should be formatted as a single string
+        const res = await fetch(`/api/upload/booru/${user.username}`, {
+            method: "POST",
+            body: data
+        })
+        if (!res.ok) {
+            setPending(false);
+            setError(res.error); // ?
+        }
+        if (res.ok) {
+            setPending(true);
+        }
+    }
+    return {image_upload, error, pending}
+}
