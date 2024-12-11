a29529800cf6217dc7cea6c487c9d1493825cf6e
newman-top <marcin.korbas.jav@gmail.com>
2024-12-10 19:55:36 -0600

    Implemented Express routes for booru image uploads

--- server/controllers/mult-controllers.js
+++ server/controllers/mult-controllers.js
@@ -1,6 +1,10 @@
 const User = require("../models/User");
 const mongoose = require("mongoose");
 
+const {
+    upload
+} = require("../middleware/mult-engine");
+
 const assign_banner = async (req, res) => {
     try {
         await User.set_banner(req.body.user, req.file.id); // Should I be instancing and assigning these variables first?
@@ -23,7 +27,29 @@ const find_banner = async (req, res) => {
     }
 }
 
+const upload_to_booru = async (req, res, next) => {
+    const user = await User.findOne({ username: req.params.user });
+    const multerware = upload(`${user._id}.imgs_full`).single("file");
+    // TODO - Need to upload thumbnail here too, somehow ...
+    multerware(req, res, next);
+}
+
+const assign_to_booru = async (req, res) => {
+    // TODO - Logic will have to implement here ...
+        // User schema will contain "booru" map which will map tags to arrays of image objects or sub-schemas
+        // So after uploading img_full and img_thumb, will need references to ObjectIDs of both documents
+        // Then will need to call a static method on the User model which creates new "booru" entries
+            // Every tag will need a reference to the image sub-schema for full and thumb res images
+                // Will also need to create an "Image" model for storing metadata in a collection
+            // If the tag does not exist as a key in the booru map, create tag and assign image
+                // The tag will map to the ObjectID of the Image document in the user's collection
+                // And the Image document will contain image details and references to full and thumb images
+    res.status(201).json({message: "Image successfully uploaded"});
+}
+
 module.exports = {
     assign_banner,
-    find_banner
+    find_banner,
+    upload_to_booru,
+    assign_to_booru
 }
--- server/routes/mult-routes.js
+++ server/routes/mult-routes.js
@@ -7,7 +7,9 @@ const {
 
 const {
     assign_banner,
-    find_banner
+    find_banner,
+    upload_to_booru,
+    assign_to_booru
 } = require("../controllers/mult-controllers");
 
 // TODO - Just making a note here cause I will need to handle this later ... MUST check that the file is an IMAGE before allowing any uploads
@@ -18,10 +20,14 @@ const {
     // i.e. If an authorized user sends a request to change a different user's banner ... that shouldn't be possible and must be prevented
 
 // Upload routes
-router.post("/upload/banner", upload("banners").single("file"), assign_banner);
+router.post("/upload/banner", upload("banners").single("file"), assign_banner); // TODO - Consider changing to use req.user.params to be consistent with other routes
 
 // ^ Logic flow is:
     // Req w/ file is received at route -> file is uploaded through multer engine -> req (now w/ _id) is handled by second callback which sets banner by ID in user document
+
+router.post("/upload/booru/:user", upload_to_booru, assign_to_booru)
+
+// TODO - The above route will also have to handle thumbnail creation eventually
 
 // Retrieval routes
 router.get("/find/banner/:user", find_banner);
