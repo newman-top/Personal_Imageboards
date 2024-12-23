f34a91965fb5130e93b79acb0f665d844402dec4
newman-top <marcin.korbas.jav@gmail.com>
2024-12-23 17:12:48 -0600

    Updated CSS for booru image grid

--- src/components/Booru.js
+++ src/components/Booru.js
@@ -5,11 +5,9 @@ const Booru = ({ booru, sv }) => {
     return (
         <div className="booru-main">
             <div className="booru-grid">
-                <div className="booru-grid-row">
-                    {booru.imgs && booru.imgs.map((img) => (
-                        <Thumbnail key={img.full} img={img} sv={sv} />
-                    ))}
-                </div>
+                {booru.imgs && booru.imgs.map((img) => (
+                    <Thumbnail key={img.full} img={img} sv={sv} />
+                ))}
             </div>
         </div>
     );
