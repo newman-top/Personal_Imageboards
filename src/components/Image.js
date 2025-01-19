a65d007b206fdee054c4c40dd31c74ca9a15d104
newman-top <marcin.korbas.jav@gmail.com>
2025-01-18 18:47:55 -0600

    Added elements for Image details to Image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -11,6 +11,10 @@ const Image = ({ img }) => {
     const { user } = useAuthContext();
     const [pending, setPending] = useState(true);
 
+    const [expand, setExpand] = useState(false); // If displayed in expanded view
+    const [editmode, setEditmode] = useState(false); // If user is editing Image details
+    const [detailmode, setDetailmode] = useState(false); // If user is viewing Image details
+
     const load_img = useLoadImage();
 
     useEffect(() => {
@@ -23,6 +27,10 @@ const Image = ({ img }) => {
         fetch_img();
     });
 
+    const show_details = () => {
+        setDetailmode(!detailmode);
+    }
+
     return (
         <div className="image-main">
             <div className="image-view">
@@ -34,6 +42,38 @@ const Image = ({ img }) => {
                     ></l-dot-stream>
                 </div>}
                 <img src="" alt="" className="image-full" id="image-full"/>
+            </div>
+            {/* TODO - Make the below details area click-to-expand (collapsed by default) */}
+            {/* Maybe only the image desc shows by default, and the others expand out */}
+            <div className="image-details" onClick={show_details}>
+                <div className="image-detail">
+                    <p>desc:</p>
+                    <p>None</p>
+                </div>
+                {detailmode && <div>
+                    <div className="image-detail">
+                        <p>author:</p>
+                        <p>None</p>
+                    </div>
+                    <div className="image-detail">
+                        <p>source:</p>
+                        <p>None</p>
+                    </div>
+                    <div className="image-detail">
+                        <p>img_date:</p>
+                        <p>None</p>
+                    </div>
+                    <div className="image-detail">
+                        <p>post_date:</p>
+                        <p>None</p>
+                    </div>
+                </div>}
+            </div>
+            <div className="image-footer">
+                <p>Image displaying at 100% resolution</p>
+                <span className="material-symbols-outlined">
+                    open_in_full
+                </span>
             </div>
         </div>
     );
